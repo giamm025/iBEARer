ed6e310c5edc2463ff33ecb4d83b7a54c8f7781a
giamm025 <francesco.giammaria@libero.it>
2026-04-09 18:11:10 +0200

    EXST: Modificato il Log per permettere attivazione/disattivazione dei singoli moduli

--- extension/utils/logger.js
+++ extension/utils/logger.js
@@ -1,10 +1,46 @@
 // prima di iniziare a impazzire con i log in ogni modulo, che una mia classe (wrapper) di console.log
+
+// configurazione per attivare/disattivare i log (il primo flag è un interruttore generale, il secondo specifica i singoli moduli)
+const LoggerConfig = {
+    log_enable: true, 
+    modules: {
+        engine: true,
+        adapter: true,
+        registry: true, 
+        intervention: true
+    }
+};
+
+// definiamo cosa il log deve stampare per ogni modulo
 const Log = {
 
-    engine: (...args) => console.log("⚙️ [Engine]", ...args),
-    registry: (...args) => console.log("📋 [Registry]", ...args),
-    adapter: (...args) => console.log("🕵️ [Adapter]", ...args),
-    intervention: (...args) => console.log("🚨 [Intervention]", ...args),
-    error: (moduleName, ...args) => console.error(`❌ [Error -> ${moduleName}]`, ...args),
-    utility: (...args) => console.log("🛠️ [Utility]", ...args)
+    engine: (...args) => {
+        if (LoggerConfig.log_enable && LoggerConfig.modules.engine) {
+            console.log("⚙️ [Engine]", ...args);
+        }
+    },
+    
+    registry: (...args) => {
+        if (LoggerConfig.log_enable && LoggerConfig.modules.registry) {
+            console.log("📋 [Registry]", ...args);
+        }
+    },
+    
+    adapter: (...args) => {
+        if (LoggerConfig.log_enable && LoggerConfig.modules.adapter) {
+            console.log("🕵️ [Adapter]", ...args);
+        }
+    },
+    
+    intervention: (...args) => {
+        if (LoggerConfig.log_enable && LoggerConfig.modules.intervention) {
+            console.log("🚨 [Intervention]", ...args);
+        }
+    },
+    
+    // gli errori chiaramente non si spengono
+    error: (moduleName, ...args) => { console.error(`❌ [Error -> ${moduleName}]`, ...args); }
 };
+
+// se il logger è abilitato, scriviamo che il logger è stato inizializzato
+if (LoggerConfig.log_enable) { console.log("🛠️ [Utility] Logger inizializzato."); }
