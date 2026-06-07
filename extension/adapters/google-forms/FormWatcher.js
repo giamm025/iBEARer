aa8de9e657c762078d6fccb041fbe905f7a7b809
giamm025 <francesco.giammaria@libero.it>
2026-06-07 16:13:51 +0200

    Modifiato il FormWatcher per osservare correttamente i cambi sezione dei questionari (es. prima, quando si passava dalla sezioen 1 alla 2, google forms salvava i dati della sezione1. veniva quindi momentaniamente caricato l'url .../formResponse facendo scattare il nostro formwatcher che chiudeva il questionario pensando avessimo finito! ora ho solo risolto questo bug)

--- extension/adapters/google-forms/FormWatcher.js
+++ extension/adapters/google-forms/FormWatcher.js
@@ -4,40 +4,51 @@
 
 console.log("🕵️ [FormWatcher] In ascolto su Google Forms...");
 
-// appena atterriamo sulla pagina di "Risposta registrata" (formResponse)
-if (window.location.href.includes("formResponse")) {
+// creiamo un timer che controlla lo stato della pagina ogni secondo per osservare se avvengono cambi di sezione
+const checkCompletion = setInterval(() => {
     
-    // estraiamo i link dei questionari 
-    chrome.storage.local.get(['preSurveyLink', 'postSurveyLink'], (data) => {
+    // appena atterriamo sulla pagina di "Risposta registrata" (formResponse)
+    if (window.location.href.includes("formResponse")) {
         
-        // facciamo parsing degli url per estrarre gli ID dei questionari
-        const { preSurveyId, postSurveyId } = extractFormIds(data);
-        const currentUrl = window.location.href;
+        // il form è veramente finito se NON ci sono ancora div con role="listitem". Altrimenti, l'utente non ha finito ma sta solo compilando un'altra sezione
+        const isReallyCompleted = document.querySelectorAll('div[role="listitem"]').length === 0;
+        if (isReallyCompleted) {
+            
+            // fermiamo il timer per non lanciare la funzione all'infinito
+            clearInterval(checkCompletion);
+            
+            // estraiamo i link dei questionari 
+            chrome.storage.local.get(['preSurveyLink', 'postSurveyLink'], (data) => {
+                
+                // facciamo parsing degli url per estrarre gli ID dei questionari
+                const { preSurveyId, postSurveyId } = extractFormIds(data);
 
-        // tramite gli idcerchiamo di capire quale questionario è stato completato 
-        let status = null;
-        if (window.location.href.includes(preSurveyId)) {
-            status = "PRE-SURVEY-COMPLETED";
-            console.log("✅ [FormWatcher] PRE-Survey completato!");
-        }
-        else if (window.location.href.includes(postSurveyId)) {
-            status = "POST-SURVEY-COMPLETED";
-            console.log("✅ [FormWatcher] POST-Survey completato!");
-        }
+                // tramite gli id cerchiamo di capire quale questionario è stato completato 
+                let status = null;
+                if (window.location.href.includes(preSurveyId)) {
+                    status = "PRE-SURVEY-COMPLETED";
+                    console.log("✅ [FormWatcher] PRE-Survey completato!");
+                }
+                else if (window.location.href.includes(postSurveyId)) {
+                    status = "POST-SURVEY-COMPLETED";
+                    console.log("✅ [FormWatcher] POST-Survey completato!");
+                }
 
-        // se abbiamo riconosciuto il questionario, aggiorniamo lo stato dell'utente 
-        if (status) {
-            chrome.runtime.sendMessage({ action: "UPDATE_STATUS", status: status }, (response) => {
-                if (response && response.success) {
-                    chrome.runtime.sendMessage({ action: "CLOSE_CURRENT_TAB" });
+                // se abbiamo riconosciuto il questionario, aggiorniamo lo stato dell'utente 
+                if (status) {
+                    chrome.runtime.sendMessage({ action: "UPDATE_STATUS", status: status }, (response) => {
+                        if (response && response.success) {
+                            chrome.runtime.sendMessage({ action: "CLOSE_CURRENT_TAB" });
+                        }
+                    });
+                    
+                } else {
+                    console.log("⚠️ [FormWatcher] Questionario sconosciuto. Nessuna azione intrapresa.");
                 }
             });
-            
-        } else {
-            console.log("⚠️ [FormWatcher] Questionario sconosciuto. Nessuna azione intrapresa.");
         }
-    });
-}
+    }
+}, 1000);
 
 // funzione helper per estrarre gli ID dei questionari dagli url
 function extractFormIds(data) {
