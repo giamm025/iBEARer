abd412ec7c50d18bdc7a32ed5876c75c4f92eb39
giamm025 <francesco.giammaria@libero.it>
2026-06-08 21:46:09 +0200

    EXST: Risolto un problema in containAny.js

--- extension/engine/operators/containsAny.js
+++ extension/engine/operators/containsAny.js
@@ -6,14 +6,38 @@ class ContainsAnyOperator extends BaseOperator {
 
     execute(actualValue, targetValues) {
 
-        // Check di consistenza: actualValue deve essere una stringa e targetValues un array
+        // check di consistenza: actualValue deve essere una stringa e targetValues un array
         if (typeof actualValue !== "string" || !Array.isArray(targetValues)) {
-            Log.error("OperatorsRegistry", `Operatore NOT_CONTAINS_ANY: tipi di dato non validi.`);
+            Log.error("OperatorsRegistry", `Operatore CONTAINS_ANY: tipi di dato non validi. Valore attuale: ${actualValue}, Valori target: ${targetValues}`);
             return false;
-        };
+        }
 
-        // ritorniamo true se actualValue contiene almeno una delle parole chiave in targetValues (ignorando maiuscole/minuscole)
-        return !targetValues.some(keyword => actualValue.toLowerCase().includes(keyword.toLowerCase()));
+        const result = targetValues.some(keyword => {
+        
+            // controlliamo se il target è una Regex (inizia e finisce con uno slash "/", con eventuali flag opzionali alla fine)
+            const regexMatch = keyword.match(/^\/(.+)\/([a-z]*)$/);
+            
+            // se non è una regex, facciamo un semplice confronto case-insensitive
+            if (!regexMatch) { return actualValue.toLowerCase().includes(keyword.toLowerCase()); }
+            
+            // altrimenti, se è una regex, costruiamo la regex e la testiamo sul valore attuale
+            try {
+                const pattern = regexMatch[1];
+                let flags = regexMatch[2];
+                
+                // siccome vogliamo tutto case-insensitive aggiungiamo la 'i' finale se il ricercatore se l'è dimenticata
+                if (!flags.includes('i')) { flags += 'i'; }
+
+                // creiamo la regex e testiamo il valore attuale
+                const regex = new RegExp(pattern, flags);
+                return regex.test(actualValue);
+
+            } catch (e) {
+                Log.error("OperatorsRegistry", `Regex non valida nel config.json: ${keyword}`);
+                return false; 
+            }
+        });
+        return result;
     }
 }   
 new ContainsAnyOperator();
