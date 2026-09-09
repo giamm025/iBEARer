357091c74182c557248c11e2885bbf2e8b16b685
giamm025 <francesco.giammaria@libero.it>
2026-09-09 15:58:59 +0200

    EXST: SearchSubmittedObserver. Spostata tutta la logica DOM dell'observer nella classe RedditAdapter

--- extension/adapters/reddit/observers/trigger/SearchSubmittedObserver.js
+++ extension/adapters/reddit/observers/trigger/SearchSubmittedObserver.js
@@ -1,10 +1,9 @@
 // creiamo un nuovo Observer che espone solo la funzione check(): responsabile di controllare se l'utente ha fatto una ricerca 
-// su Reddit e di emettere l'evento corrispondente (SearchSubmitted)
+// ed emettere l'evento corrispondente (SearchSubmitted)
 
 class SearchSubmittedObserver extends BaseTriggerObserver {
 
     constructor() {
-        
         // per estrarre il nome dell'evento che questo observer deve intercettare, creiamo un evento fittizio e ne estraiamo il FQN
         const event = new SearchSubmittedEvent();
         super(event.eventFqn);
@@ -15,17 +14,12 @@ class SearchSubmittedObserver extends BaseTriggerObserver {
     }
 
     check() {
-
+        // se siamo su una pagina di ricerca => estriamo la query di ricerca => creiamo l'evento custom => emettiamo l'evento
         if (!this.isActive) return;
-
-        // creiamo un URLSearchParams per leggere i parametri dall'URL (es. ?q=conspiracy)
-        const urlParams = new URLSearchParams(window.location.search);
-        
-        // per come funziona reddit le ricerche vengono fatte su .../search e la query specifica inserita nel parametro "q"
-        if (window.location.pathname.includes('/search') && urlParams.has('q')) {
-            const query = urlParams.get('q');                                           // estraiamo la query di ricerca
-            const searchEvent = new SearchSubmittedEvent({search_query: query});        // creiamo un nuovo evento " L'utente ha cercato *query* "
-            document.dispatchEvent(searchEvent);                                        // emettiamo l'evento
+        if (PlatformAdapter.isSearchPage()) {
+            const query = PlatformAdapter.getCurrentSearchQuery();                                          
+            const searchEvent = new SearchSubmittedEvent({search_query: query});        
+            document.dispatchEvent(searchEvent);                                        
         }
     }
 };
